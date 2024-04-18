415310eaf3aabe199155c5303172e94889bc40cc
kaantrz <93608073+kaantrz@users.noreply.github.com>
2024-04-18 14:38:55 +0300

    Create EventData.java

---
+++ EventData.java
@@ -0,0 +1,28 @@
+import java.io.Serializable;
+
+public class Data implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String message;
+
+    public Data(int id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
